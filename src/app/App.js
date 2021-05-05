8ba6028832b108cd61ea1267d16987fb0d7adc5d
Leonardo Castro <castroleonardo@live.cl>
2021-05-05 12:57:19 -0400

    integrado webpack, react para generacion del app en public

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+        return(
+           <div>
+               <p>Hola MunDo!</p>
+           </div>
+        );
+    }
+}
+
+export default App;
