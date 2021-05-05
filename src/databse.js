f5ef96d175673512d8ab4013e19f76c3d030f056
Leonardo Castro <castroleonardo@live.cl>
2021-05-04 21:40:08 -0400

    generado env

--- src/databse.js
+++ src/databse.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv/config');
 
-const URI = 'mongodb+srv://db_user_platzi_videos:nissa1304@cluster0.enqzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const USER = process.env.DB_USER;
+const PASS = process.env.DB_PASSWORD;
+
+const URI = `mongodb+srv://${USER}:${PASS}@cluster0.enqzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 mongoose.connect(URI).then(db => console.log('DB is connected')).catch(err => console.error(err));
 
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -13,6 +13,14 @@ router.post('/', async (req, res) => {
     const taskOne = new Task({title, description});
     await taskOne.save();
     res.json({status: "task saved"});
+});
+
+router.put('/:id', async (req, res) => {
+    const { title, description } = req.body;
+    const updateTask = { title, description };
+    await Task.findByIdAndUpdate(req.params.id, updateTask);
+    res.json({status: "task updated"});
+
 })
 
 module.exports = router;
