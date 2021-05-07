d00711c585a6b1340242da36e7af6f415cc4ed9f
Leonardo Castro <castroleonardo@live.cl>
2021-05-06 21:01:47 -0400

    continuando API REST

--- src/app/App.js
+++ src/app/App.js
@@ -6,10 +6,23 @@ class App extends React.Component {
         super();
         this.state = {
             title: '',
-            description: ''
+            description: '',
+            tasks: []
         };
         this.addTask = this.addTask.bind(this);
         this.handleChange = this.handleChange.bind(this);
+    }
+
+    fetchTasks() {
+        fetch('/api/tasks', {
+            method: 'GET',
+        })
+        .then(res => res.json())
+        .then(data => {
+            this.setState({ tasks: data });
+            console.log(this.state.tasks);
+        })
+        .catch(err => console.error(err));
     }
 
     addTask(e) {
@@ -27,7 +40,8 @@ class App extends React.Component {
            M.toast({
                html: 'Task Saved'
            });
-           this.setState({ title: '', description: ''})
+           this.setState({ title: '', description: ''});
+           this.fetchTasks();
        })
        .catch(err => console.log(err));
         e.preventDefault();
@@ -38,6 +52,10 @@ class App extends React.Component {
         this.setState({
             [name]: value
         });
+    }
+
+    componentDidMount() {
+        this.fetchTasks();
     }
 
     render(){
@@ -73,7 +91,26 @@ class App extends React.Component {
                             </div>
                        </div>
                        <div className="col s7">
-                           
+                           <table>
+                               <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                    </tr>
+                               </thead>
+                               <tbody>
+                                    {
+                                        this.state.tasks.map(task => {
+                                            return(
+                                                <tr key={task._id}>
+                                                    <td>{task.title}</td>
+                                                    <td>{task.description}</td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
+                               </tbody>
+                           </table>
                        </div>
                    </div>
                </div>
