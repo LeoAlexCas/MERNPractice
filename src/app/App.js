f883c2884cc5ae5394debbfbe716592ef0633ed1
Leonardo Castro <castroleonardo@live.cl>
2021-05-06 22:59:45 -0400

    agregado API REST

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,7 @@ class App extends React.Component {
     constructor(){
         super();
         this.state = {
+            _id: '',
             title: '',
             description: '',
             tasks: []
@@ -26,25 +27,79 @@ class App extends React.Component {
     }
 
     addTask(e) {
-       fetch('/api/tasks', {
-           method: 'POST',
-           body: JSON.stringify(this.state),
-           headers: {
-            'Accpet': 'application/json',   
-            'Content-Type': 'application/json'
-           }
-       })
-       .then(res => res.json())
-       .then(data => {
-           console.log(data);
-           M.toast({
-               html: 'Task Saved'
-           });
-           this.setState({ title: '', description: ''});
-           this.fetchTasks();
-       })
-       .catch(err => console.log(err));
+        if(this.state._id) {
+            fetch(`/api/tasks/${this.state._id}`, {
+                method: 'PUT',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Accpet': 'application/json',   
+                    'Content-Type': 'application/json'
+                   }
+            })
+            .then(res => res.json())
+            .then(data => {
+                M.toast({
+                    html: 'Task Updated'
+                });
+                console.log(data);
+                this.setState({_id: '', title: '', description: ''});
+                this.fetchTasks();
+            })
+            .catch(err => console.error(err));
+        } else {
+            fetch('/api/tasks', {
+                method: 'POST',
+                body: JSON.stringify(this.state),
+                headers: {
+                 'Accpet': 'application/json',   
+                 'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                M.toast({
+                    html: 'Task Saved'
+                });
+                this.setState({ title: '', description: ''});
+                this.fetchTasks();
+            })
+            .catch(err => console.log(err));
+        }
         e.preventDefault();
+    }
+
+    deleteTask(id) {
+        if(confirm('Are you sure you want to Delete?')){
+            fetch(`/api/tasks/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accpet': 'application/json',   
+                    'Content-Type': 'application/json'
+                   }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                M.toast({
+                    html: 'Task Deleted'
+                });
+                this.fetchTasks();
+            })
+            .catch(err => console.error(error));
+        }
+    }
+
+    editTask(id) {
+        fetch(`/api/tasks/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                _id: data._id,
+                title: data.title,
+                description: data.description
+            })
+        })
     }
 
     handleChange(e) {
@@ -105,6 +160,18 @@ class App extends React.Component {
                                                 <tr key={task._id}>
                                                     <td>{task.title}</td>
                                                     <td>{task.description}</td>
+                                                    <td>
+                                                        <button className='btn light-blue darken-4' onClick={() => {
+                                                            this.editTask(task._id);
+                                                        }}>
+                                                            <i className='material-icons'>edit</i>
+                                                        </button>
+                                                        <button className='btn light-blue darken-4'  onClick={() => {
+                                                            this.deleteTask(task._id);
+                                                        }}>
+                                                            <i className='material-icons'>delete</i>
+                                                        </button>
+                                                    </td>
                                                 </tr>
                                             );
                                         })
